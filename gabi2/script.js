ee9bac503cb4e2242e48e37e7cbeec269a11ff33
Gabi <116088564+gabrielipototski@users.noreply.github.com>
2024-05-27 08:47:33 -0300

    Add files via upload

---
+++ gabi2/script.js
@@ -0,0 +1,28 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoresultao = document.querySelector(".texto-resultado");
+
+
+const perguntas = [
+    {
+
+    
+   enunciado: "Pergunta 1", 
+   alternativas: [
+    "Alternativa1",
+     "Alternativa2"
+     ]
+ }
+
+ {
+
+    
+    enunciado: "Pergunta 1", 
+    alternativas: [
+     "Alternativa1",
+      "Alternativa2"
+      ]
+  }
+    ]
